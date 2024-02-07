65052c40a3b3ad9b8235f803c0e25c5e543b6ff4
artforme <me.peremy@gmail.com>
2024-02-07 23:01:49 +0300

    create file that contains initialization of gRPC server app

---
+++ internal/app/grpc/grpcapp.go
@@ -0,0 +1,28 @@
+package grpcapp
+
+import (
+	"google.golang.org/grpc"
+	"log/slog"
+	authgrpc "sso/internal/grpc/auth"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+// New creates new gRPC server app.
+func New(
+	log *slog.Logger,
+	port int,
+) *App {
+	gRPCServer := grpc.NewServer()
+	authgrpc.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
