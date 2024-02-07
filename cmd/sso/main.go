94c7966752ea0d08d83ca23d91dc423f8c3d5adb
artforme <me.peremy@gmail.com>
2024-02-07 19:28:43 +0300

    feat: setup logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,5 +1,47 @@
-package sso
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
+	cfg := config.MustLoad()
 
+	log := SetupLogger(cfg.Env)
+
+	log.Info("config", slog.Any("cfg", cfg))
+
+	log.Debug("debug message")
+
+	log.Info("info message")
+
+	log.Warn("warn message")
+}
+
+// SetupLogger set the type and level of logger depending on env
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
